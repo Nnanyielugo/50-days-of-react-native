d25d6d7dd29bedb39c93f462b4f29571915b5079
Nnanyielugo <kz.onyegbule@gmail.com>
2021-07-19 19:43:18 +0100

    upgrade react-native to 0.64.1

---
+++ ios/Pods/Target Support Files/Pods-fiftyDays-fiftyDaysTests/Pods-fiftyDays-fiftyDaysTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fiftyDays_fiftyDaysTests : NSObject
+@end
+@implementation PodsDummy_Pods_fiftyDays_fiftyDaysTests
+@end
--- src/Containers/StopWatch.js
+++ src/Containers/StopWatch.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import {
-	View,
-	StyleSheet,
+  View,
+  StyleSheet,
   Dimensions,
-  ScrollView
+  ScrollView,
+  SafeAreaView,
 } from 'react-native';
 
 import Timer from '../Components/StopWatch/Timer';
@@ -13,61 +14,61 @@ import format from '../Components/StopWatch/utils';
 import Button from '../Components/StopWatch//Buttons';
 
 class Index extends Component {
-	getInitialState = () => ({
-		timeElapsed: 0,
-		laps: [],
+  getInitialState = () => ({
+    timeElapsed: 0,
+    laps: [],
     running: false,
-    lastLap: 0
-	})
+    lastLap: 0,
+  });
 
-	state = this.getInitialState()
+  state = this.getInitialState();
 
   start = () => {
     const { running } = this.state;
     if (!running) {
       this.timing = Date.now();
       this.timer = setInterval(this.update, 100);
-      this.setState({ running: true })
+      this.setState({ running: true });
     }
-  }
+  };
 
   update = () => {
     let diff = Date.now() - this.timing;
     this.setState(state => ({
       timeElapsed: state.timeElapsed + diff,
-      lastLap: state.lastLap + diff
+      lastLap: state.lastLap + diff,
     }));
     this.timing = Date.now();
-  }
+  };
 
-	split = () => {
-		this.setState(state => ({
+  split = () => {
+    this.setState(state => ({
       laps: state.laps.concat(state.timeElapsed),
-      lastLap: this.getInitialState().lastLap
-    }))
-	}
+      lastLap: this.getInitialState().lastLap,
+    }));
+  };
 
-	stop = () => {
-    clearInterval(this.timer)
-		this.setState({
-			running: false
-    })
-	}
+  stop = () => {
+    clearInterval(this.timer);
+    this.setState({
+      running: false,
+    });
+  };
 
-	reset = () => {
-    clearInterval(this.timer)
-		this.setState(() => ({
-			...this.getInitialState()
-		}))
-	}
+  reset = () => {
+    clearInterval(this.timer);
+    this.setState(() => ({
+      ...this.getInitialState(),
+    }));
+  };
 
-	render () {
-		const { timeElapsed, laps, running, lastLap } = this.state;
-		return (
-			<View style={styles.container}>
+  render() {
+    const { timeElapsed, laps, running, lastLap } = this.state;
+    return (
+      <SafeAreaView style={styles.container}>
         <ScrollView contentContainerStyle={styles.scrollContainer}>
           <View style={styles.header}>
-            <Timer 
+            <Timer
               time={format(timeElapsed)}
               lastLap={format(lastLap)}
               lastLapColor={running ? 'red' : 'grey'}
@@ -81,7 +82,7 @@ class Index extends Component {
               running={running}
               startCOlor={running ? 'grey' : 'blue'}
             />
-            <View style={{ width: Dimensions.get('screen').width * .9}}>
+            <View style={{ width: Dimensions.get('screen').width * 0.9 }}>
               <LapList laps={laps} />
             </View>
           </View>
@@ -95,57 +96,57 @@ class Index extends Component {
             textColor="white"
             style={{
               container: styles.cancelButton,
-              text: styles.cancelButtonText
+              text: styles.cancelButtonText,
             }}
             onPress={this.reset}
           />
         </View>
-			</View>
-		)
-	}
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
+  container: {
     backgroundColor: 'whitesmoke',
     alignItems: 'center',
   },
   scrollContainer: {
     flexGrow: 1,
     alignItems: 'center',
-    paddingBottom: 90
+    paddingBottom: 90,
   },
   timerHeader: {
     fontSize: 30,
     fontWeight: '600',
     marginLeft: 20,
     marginTop: 25,
-    marginBottom: 15
+    marginBottom: 15,
   },
   body: {
-    paddingTop: 15
+    paddingTop: 15,
   },
   header: {
     backgroundColor: 'white',
-    width: Dimensions.get('screen').width
+    width: Dimensions.get('screen').width,
   },
-  cancelContainer: { 
-    position: 'absolute', 
+  cancelContainer: {
+    position: 'absolute',
     bottom: 0,
     width: Dimensions.get('screen').width,
     paddingBottom: 20,
-    backgroundColor: 'whitesmoke'
+    backgroundColor: 'whitesmoke',
   },
   cancelButton: {
     marginVertical: 10,
     marginHorizontal: 10,
     paddingVertical: 15,
     alignItems: 'center',
-    borderRadius: 5
+    borderRadius: 5,
   },
   cancelButtonText: {
-    fontSize: 19
-  }
-})
+    fontSize: 19,
+  },
+});
 
 export default Index;
