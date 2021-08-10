564aa5c58500bd2202d86385cfb184bf28b13e97
Nnanyielugo <kz.onyegbule@gmail.com>
2021-08-10 10:15:36 +0100

    init music player

--- src/Containers/MusicPlayer.tsx
+++ src/Containers/MusicPlayer.tsx
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 
-interface ComponentState {}
+interface Track {
+  id: number;
+  title: string;
+  url: string;
+  artwork: string;
+  artist: string;
+}
+
+interface ComponentState {
+  tracks: Track[];
+}
 
 class MusicPlayer extends Component<{}, ComponentState> {
+  state = {
+    tracks: [],
+  };
+
+  startPlayer = async () => {
+    console.log('tracks', this.state.tracks);
+    const { tracks } = this.state;
+    await TrackPlayer.setupPlayer();
+    await TrackPlayer.updateOptions({
+      // stopWithApp: true,
+      capabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+        TrackPlayer.CAPABILITY_STOP,
+      ],
+      compactCapabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      ],
+    });
+    await TrackPlayer.add(tracks);
+    // await TrackPlayer.play();
+  };
+  componentDidMount() {
+    const url2 = 'https:/api.deezer.com/chart/0?limit=30';
+    fetch(url2)
+      .then(res => res.json())
+      .then(response => {
+        const tracks: Track[] = [];
+        console.log('resp', response);
+        response.tracks.data.map((track: any) => {
+          return tracks.push({
+            id: track.id,
+            title: track.title,
+            url: track.preview,
+            artwork: track.md5_image,
+            artist: track.artist.name,
+          });
+        });
+        this.setState(
+          _state => ({
+            tracks,
+          }),
+          () => this.startPlayer(),
+        );
+      });
+  }
   render() {
     return (
       <View style={styles.container}>
