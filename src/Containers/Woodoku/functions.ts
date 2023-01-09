76c89e6bc885e4c86b0393f206ab2bba4463794e
Nnanyielugo <kz.onyegbule@gmail.com>
2023-01-09 06:45:56 +0100

    generate board

--- src/Containers/Woodoku/functions.ts
+++ src/Containers/Woodoku/functions.ts
@@ -1,23 +1,11 @@
-import * as uuid from 'uuid';
-
-import {
-  getWidthsForTwos,
-  getWidthsForThrees,
-  getWidthsForFours,
-  BOARD_WIDTH,
-} from './constants';
+import { BOARD_WIDTH, getBoards } from './constants';
 import type { RowObj, BrickObj } from './types';
 
 export function generateId(): string {
   let id =
-    uuid.v4() +
+    (Math.random() * new Date().getTime()).toString() +
     '-' +
-    Math.random() * new Date().getTime() +
-    new Date().getTime().toString() +
-    '-' +
-    uuid.v1() +
-    uuid.v4();
-
+    (Math.random() * 1e4).toString();
   return id;
 }
 
@@ -30,49 +18,71 @@ function generateBrickNum() {
   return numBricks;
 }
 
-function pickWidthForTwos() {
-  const widths = getWidthsForTwos();
+// function pickWidthForTwos() {
+//   const widths = getWidthsForTwos();
 
-  let brickNum = Math.floor(Math.random() * widths.length);
-  return widths[brickNum];
-}
-function pickWidthForThrees() {
-  const widths = getWidthsForThrees();
+//   let brickNum = Math.floor(Math.random() * widths.length);
+//   return widths[brickNum];
+// }
+// function pickWidthForThrees() {
+//   const widths = getWidthsForThrees();
 
-  let brickNum = Math.floor(Math.random() * widths.length);
-  return widths[brickNum];
-}
-function pickWidthForFours() {
-  const widths = getWidthsForFours();
-  let brickNum = Math.floor(Math.random() * widths.length);
-  return widths[brickNum];
-}
+//   let brickNum = Math.floor(Math.random() * widths.length);
+//   return widths[brickNum];
+// }
+// function pickWidthForFours() {
+//   const widths = getWidthsForFours();
+//   let brickNum = Math.floor(Math.random() * widths.length);
+//   return widths[brickNum];
+// }
 
-export function generateRow(): RowObj {
-  let output: RowObj;
-  let numBricks = generateBrickNum();
-  pickWidthForFours();
+// export function generateRow(): RowObj {
+//   let output: RowObj;
+//   let numBricks = generateBrickNum();
+//   pickWidthForFours();
 
-  if (numBricks === 2) {
-    output = pickWidthForTwos();
-  } else if (numBricks === 3) {
-    output = pickWidthForThrees();
-  } else {
-    output = pickWidthForFours();
+//   if (numBricks === 2) {
+//     output = pickWidthForTwos();
+//   } else if (numBricks === 3) {
+//     output = pickWidthForThrees();
+//   } else {
+//     output = pickWidthForFours();
+//   }
+
+//   return output;
+// }
+
+export function generateRows(): RowObj[] {
+  // const boards = getBoards()
+  let board = [];
+  let boardNum = Math.floor(Math.random() * 8);
+  console.log('ranndo', boardNum);
+
+  board = getBoards(boardNum);
+  // let board = boards[boardNum];
+
+  for (let i = 0; i < board.length; i++) {
+    const row = board[i];
+
+    for (let j = 0; j < row.row.length; j++) {
+      const brick = row.row[j];
+      const pos = getBrickPosition(brick, row);
+      board[i].row[j].pos = pos;
+    }
   }
 
-  return output;
-}
-
-export function generateRows(rowNum: number): RowObj[] {
-  const rows: RowObj[] = [];
-
-  for (let i = 0; i < rowNum; i++) {
-    let row = rearrangeRowSpacing(generateRow());
-    rows.push(row);
+  for (let i = 0; i < board.length; i++) {
+    let row = board[i];
+    let visibleRow = row.row.filter(brick => !brick.transparent);
+    board.splice(i, 1, {
+      id: row.id,
+      row: visibleRow,
+    });
   }
 
-  return rows;
+  console.log('board', board, boardNum);
+
+  return board;
 }
 
 function rearrangeRowSpacing(row: RowObj): RowObj {
@@ -106,4 +116,76 @@ function rearrangeRowSpacing(row: RowObj): RowObj {
   return row;
 }
 
-// export function fillDropRows(rows: RowObj[]): RowObj[] {}
+function handleDropDown(
+  brick: BrickObj,
+  row: RowObj,
+  rowIndex: number,
+  rows: RowObj[],
+) {
+  // if (rowIndex === rows)
+}
+
+// returns information about the space and item to drop into (the under item)
+export function canDropDown(
+  target: BrickObj,
+  currentRow: RowObj,
+  rowUnder: RowObj,
+) {
+  const targetBrickPosition = getBrickPosition(target, currentRow);
+  // console.log('target pos', targetBrickPosition, currentRow, rowUnder);
+  for (let i = 0; i < rowUnder.row.length; i++) {
+    let brick = rowUnder.row[i];
+    let brickPosition = getBrickPosition(brick, rowUnder);
+    const underLapping =
+      targetBrickPosition.left >= brickPosition.left &&
+      targetBrickPosition.right <= brickPosition.right;
+
+    if (underLapping && brick.transparent) {
+      // console.log('------------UNDER_LAPPING------------');
+      // console.log(targetBrickPosition, target, brickPosition, brick);
+      // console.log();
+      return {
+        pos: brickPosition,
+        index: i,
+        brick: brick,
+      };
+    }
+  }
+
+  return;
+}
+
+export function getBrickPosition(target: BrickObj, currentRow: RowObj) {
+  const brickIndex = currentRow.row.findIndex(
+    rowItem => rowItem.id === target.id,
+  );
+  let left: number = 0;
+  let right: number = 0;
+  let accRight = 0;
+  if (brickIndex === 0) {
+    left = 0;
+  } else {
+    for (let i = 0; i < currentRow.row.length; i++) {
+      if (i === brickIndex) {
+        break;
+      }
+
+      left += currentRow.row[i].width;
+    }
+  }
+
+  if (brickIndex === currentRow.row.length - 1) {
+    right = BOARD_WIDTH;
+  } else {
+    for (let i = currentRow.row.length - 1; i > 0; i--) {
+      if (i === brickIndex) {
+        break;
+      }
+
+      accRight += currentRow.row[i].width;
+      right = BOARD_WIDTH - accRight;
+    }
+  }
+
+  return { left, right };
+}
--- src/Containers/Woodoku/types.ts
+++ src/Containers/Woodoku/types.ts
@@ -1,7 +1,13 @@
+export type BrickPos = {
+  left: number;
+  right: number;
+};
+
 export type BrickObj = {
   id: string;
   width: number;
   transparent: boolean;
+  pos?: BrickPos;
 };
 
 export type RowObj = {
