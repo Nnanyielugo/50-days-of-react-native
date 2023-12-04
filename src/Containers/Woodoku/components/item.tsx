3ef6dbe6252d614042bde226e64c0982d1d2fca5
Nnanyielugo <kz.onyegbule@gmail.com>
2023-12-04 16:20:00 +0100

    cleanup

--- src/Containers/Woodoku/components/item.tsx
+++ src/Containers/Woodoku/components/item.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, PanResponder, Animated } from 'react-native';
 
 import Brick from '../assets/brick.jpeg';
